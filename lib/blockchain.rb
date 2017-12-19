4053ff2dc446ec733a70880b42f1c43eba8b40a3
Suzanne Huldt <suzanne.huldt.16@ucl.ac.uk>
2017-12-19 14:56:52 +0000

    new transaction and new block working

--- lib/blockchain.rb
+++ lib/blockchain.rb
@@ -1,6 +1,8 @@
-require './block.rb'
+require 'block.rb'
+require 'transaction.rb'
 
 class Blockchain
+  attr_accessor :chain, :current_transactions
   def initialize
     @chain = []
     @current_transactions = []
@@ -8,22 +10,24 @@ class Blockchain
 
   #remember to def genesis block
 
-  def new_block(self, proof, previous_hash=None)
-    @block = Block.new()
+  def new_block(x, y, z, a, b)
+    @chain.push(Block.new(x, y, z, a, b))
   end
 
-  def new_transaction(self, sender, recipient, amount)
-    #create new transaction
-
-    #append then return index of block to be added to
+  def new_transaction(sender, recipient, amount)
+    @transactions.push(transaction = Transaction.new(sender, recipient, amount)) #create new transaction
+    return last_block
+    #append then return index of block to be added to - as this TRANSACTION goes into
+    # the NEXT block to be created -> it is stored in the transaction array so that it
+    #- or rather its hash - can be appeneded
   end
 
   def last_block
-    return @chain.index(-1)
+    return @chain.last
   end
 
   def hash(block)
-    
+    #hashes block
   end
 
 end
--- spec/blockchain_spec.rb
+++ spec/blockchain_spec.rb
@@ -1,1 +1,18 @@
-require './lib/blockchain.rb'
+require 'blockchain.rb'
+
+describe Blockchain do
+  describe "#new_block" do
+    it "creates a new instance of the block class" do
+      blockchain = Blockchain.new
+      blockchain.new_block("some", "stuff", "in", "here", "again")
+      expect(blockchain.chain.last).to be_a Block
+    end
+    it "appends the block to the chain" do
+      blockchain = Blockchain.new
+      blockchain.new_block("some", "stuff", "in", "here", "again")
+      expect(blockchain.chain.length).to be 1
+    end
+  end
+
+
+end
